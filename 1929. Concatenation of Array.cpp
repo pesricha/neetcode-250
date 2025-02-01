d3c856e7f8b4c756a1a55e80390b81748e2a77a9
pesricha <naman_p@me.iitr.ac.in>
2025-02-01 19:11:26 +0530

    32/250

--- 1929. Concatenation of Array.cpp
+++ 1929. Concatenation of Array.cpp
@@ -7,8 +7,8 @@ public:
         int n = nums.size();
         vector<int> result(2 * n);
         for (int i = 0; i < n; ++i) {
-            result[i] = nums[i];
-            result[i + n] = nums[i];
+            result[i] = nums[i] ;
+            result[i + n] = nums[i] ;
         }
         return result;
     }   
